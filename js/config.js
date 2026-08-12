df857285d334db8dae445e842abdae5d60d96d49
Paul Janssen <p.janssen@geonovum.nl>
2026-08-12 12:10:26 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,8 +6,8 @@ let respecConfig = {
   title: "Informatiemodel Mobiliteit (IMMOB)",
   subtitle: "Gegevenscatalogus Mobiliteitsdata",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "wv",                   // Werkversie
-  specStatus: "cv",               // Consultatieversie
+  specStatus: "wv",                   // Werkversie
+ //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
@@ -37,7 +37,7 @@ let respecConfig = {
 
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2026-06-16",
+ // publishDate: "2026-06-16",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: [],
   //previousVersion: [],
